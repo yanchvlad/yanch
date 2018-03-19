9252872c5f88129b3a821a8ccda1e8b985d507d2
yanchvlad <36820904+yanchvlad@users.noreply.github.com>
2018-03-19 17:06:44 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yanch',
+      version='0.1',
+      description='lib',
+      #url='https://github.com/yanchvlad/yanch',
+      author='Vladislav Yanchenko',
+      author_email='later',
+      license='MIT',
+      packages=['yanch'],
+      zip_safe=False)
